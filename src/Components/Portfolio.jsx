285cdd128c34c60137467eafb3edd96cf5857ade
Burhanuddin Khatri <144617735+BurhanCantCode@users.noreply.github.com>
2023-11-15 17:14:54 +0000

    My first portfolio, all done!

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Learning For A Sustainable Future by University Of Edinburgh🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://www.linkedin.com/posts/burhanuddin-khatri-aa44a8247_completion-certificate-for-learning-for-a-activity-7129196482041135104-LNfn?utm_source=share&utm_medium=member_desktop",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Coders Cup",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "https://www.linkedin.com/posts/burhanuddin-khatri-aa44a8247_coders-cup-certificate-activity-7130211886444257280-XuxR?utm_source=share&utm_medium=member_desktop",
   },
   {
-    title: "My Resume Site",
+    title: "My Github Repos",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/BurhanCantCode?tab=repositories",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Git and GitHub",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "In person review of the seminar held at FAST NUCES.",
+    url: "https://www.linkedin.com/posts/burhanuddin-khatri-aa44a8247_excited-to-announce-that-i-have-successfully-activity-7126147647756775424-499v?utm_source=share&utm_medium=member_desktop",
   },
 ];
 
